45de801de97255fedcb771a09cbe1a8d75fcf53e
Altakie <artemlavrov@brandeis.edu>
2026-05-09 18:08:14 -0400

    Added websockets and dynamically updating count

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,48 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type SetStateAction } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { useQuery, useMutation, QueryClientProvider, QueryClient, useQueryClient } from "@tanstack/react-query"
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [msg, setMessage] = useState("")
+  const [input, setInput] = useState("")
 
+  const queryClient = useQueryClient();
 
+  const { mutate: updateCount } = useMutation(
+    {
+      mutationKey: ['count'],
+      mutationFn: async (count: number) => {
+        return await fetch("/count", {
+          method: "POST",
+          body: count.toString(),
+        })
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['count'] })
+      }
+    }
+  )
+
+  const ws = new WebSocket("/socket")
+  ws.onopen = () => {
+    console.log("Opened Connection!")
+  }
+
+  ws.onmessage = (ev) => {
+    let json = JSON.parse(ev.data)
+    setMessage(json.msg)
+    setCount(json.count)
+    queryClient.invalidateQueries({
+      queryKey: ['count']
+    })
+  }
+
+  // ws.send("Skeeby Deeby")
 
   return (
     <>
@@ -19,131 +54,181 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <p>Server Message: {msg}</p>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
+        <input value={input} onChange={(e) => setInput(e.target.value)}></input>
+        <button onClick={() => {
+          ws.send(input)
+          setInput("")
+        }}>WebSocket Send</button>
         <button
           type="button"
           className="counter"
-          onClick={() => setCount((count) => count + 10)}
+          onClick={() => {
+            updateCount(count + 1)
+            setCount(count + 1)
+          }
+          }
         >
-          Count is {count}
+          <ServerCount setCount={setCount}>
+          </ServerCount>
+          {/* Count is {count} */}
         </button>
         <p>Server says <ServerRes /></p>
+
+        <div className="ticks"></div>
+
+        <section id="next-steps">
+          <div id="docs">
+            <svg className="icon" role="presentation" aria-hidden="true">
+              <use href="/icons.svg#documentation-icon"></use>
+            </svg>
+            <h2>Documentation</h2>
+            <p>Your questions, answered</p>
+            <ul>
+              <li>
+                <a href="https://vite.dev/" target="_blank">
+                  <img className="logo" src={viteLogo} alt="" />
+                  Explore Vite
+                </a>
+              </li>
+              <li>
+                <a href="https://react.dev/" target="_blank">
+                  <img className="button-icon" src={reactLogo} alt="" />
+                  Learn more
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div id="social">
+            <svg className="icon" role="presentation" aria-hidden="true">
+              <use href="/icons.svg#social-icon"></use>
+            </svg>
+            <h2>Connect with us</h2>
+            <p>Join the Vite community</p>
+            <ul>
+              <li>
+                <a href="https://github.com/vitejs/vite" target="_blank">
+                  <svg
+                    className="button-icon"
+                    role="presentation"
+                    aria-hidden="true"
+                  >
+                    <use href="/icons.svg#github-icon"></use>
+                  </svg>
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a href="https://chat.vite.dev/" target="_blank">
+                  <svg
+                    className="button-icon"
+                    role="presentation"
+                    aria-hidden="true"
+                  >
+                    <use href="/icons.svg#discord-icon"></use>
+                  </svg>
+                  Discord
+                </a>
+              </li>
+              <li>
+                <a href="https://x.com/vite_js" target="_blank">
+                  <svg
+                    className="button-icon"
+                    role="presentation"
+                    aria-hidden="true"
+                  >
+                    <use href="/icons.svg#x-icon"></use>
+                  </svg>
+                  X.com
+                </a>
+              </li>
+              <li>
+                <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                  <svg
+                    className="button-icon"
+                    role="presentation"
+                    aria-hidden="true"
+                  >
+                    <use href="/icons.svg#bluesky-icon"></use>
+                  </svg>
+                  Bluesky
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <div className="ticks"></div>
+        <section id="spacer"></section>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
 
 function ServerRes() {
-  const [res, setRes] = useState("");
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["Server"], queryFn: async () => {
+      // const ws = new WebSocket("http://localhost:3000/api")
+      // ws.onmessage = (event) => {
+      //   new Response(event.data).text().then((s) => {
+      //     setRes(s)
+      //   })
+      // }
+      //
+      // return () => ws.close()
 
-  useEffect(() => {
-    // const ws = new WebSocket("http://localhost:3000/api")
-    // ws.onmessage = (event) => {
-    //   new Response(event.data).text().then((s) => {
-    //     setRes(s)
-    //   })
-    // }
-    //
-    // return () => ws.close()
+      return fetch("/api").then((r) => r.text().then(
+        (t) => {
+          return t
+        }
+      ))
+    }
+  })
 
-    fetch("http://localhost:3000/api").then((r) => r.text().then(
-      (t) => {
-        setRes(t)
-      }
-    ))
-  }, [])
+  if (isPending) {
+    return "Pending"
+  }
 
-  return res
+  if (isError) {
+    return "Error"
+  }
+
+  return data
 }
+
+
+
+function ServerCount({ setCount }) {
+  const { isFetching, data } = useQuery(
+    {
+      queryKey: ['count'],
+      queryFn: async () => {
+        // await new Promise((r) => setTimeout(r, 1000))
+        return fetch("/count").then((r) => r.text())
+      }
+    }
+  )
+
+  if (isFetching) {
+    return <Loading></Loading>
+  }
+
+  setCount(parseInt(data))
+  return <h2>{data}apus</h2>
+}
+
+function Loading() {
+  return <h2>Loading...adeek</h2>;
+}
+
+// function Socket({ws}) {
+//   ws.send("Skeeby Deeby")
+// }
+
 
 
 
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { serveStatic } from "hono/bun";
+import { serveStatic, websocket } from "hono/bun";
+import { upgradeWebSocket } from "hono/bun";
+import { format } from "util";
+import { WSContext } from "hono/ws";
 
 const app = new Hono()
 
@@ -12,14 +15,65 @@ const app = new Hono()
 
 app.use(cors())
 
+let count = 0
+
 app.get("/api", (c) => {
-  return c.text("Hi!")
+  return c.text("Hi! " + count)
 })
 
+app.get("/count", (c) => {
+  return c.text(count.toString())
+})
+
+let webby: WSContext;
+
+app.post("/count", async (c) => {
+  let text = await c.req.text()
+  count = parseInt(text)
+  webby.send(JSON.stringify({
+    msg: "Here's the count!",
+    count: count
+  }))
+  return c.text(count.toString())
+})
+
+app.use("/socket", upgradeWebSocket((c) => {
+  return {
+    onOpen: async (event, ws) => {
+      webby = ws
+    },
+    onMessage: async (event, ws) => {
+      let msg = event.data.toString()
+      console.log("Message data: %s", msg)
+      let json = JSON.stringify({
+        msg: "Hi Client!",
+        count: count
+      })
+      ws.send(json)
+      await new Promise((r) => setTimeout(r, 1000))
+      json = JSON.stringify({
+        msg: format("Your message was %s characters long", msg.length),
+        count: count
+      })
+      ws.send(json)
+    },
+    onClose: () => {
+      console.log("Closed :(")
+    }
+  }
+}))
+
+
 app.use("/*", serveStatic({ root: "../client/dist" }))
+
 
 // app.get('/', (context) => {
 //   return context.text("Hi!")
 // })
 
-export default app
+export default {
+  hostname: '0.0.0.0',
+  port: 3000,
+  fetch: app.fetch,
+  websocket,
+}
