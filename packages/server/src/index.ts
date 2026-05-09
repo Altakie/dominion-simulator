3454c7c23ab26d1e86c66780d93005a79d3c0dbc
Altakie <artemlavrov@brandeis.edu>
2026-05-09 18:47:53 -0400

    Synced count between all clients using map of websockets to clientids

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,6 +4,7 @@ import { serveStatic, websocket } from "hono/bun";
 import { upgradeWebSocket } from "hono/bun";
 import { format } from "util";
 import { WSContext } from "hono/ws";
+import { setCookie, getCookie } from "hono/cookie";
 
 const app = new Hono()
 
@@ -16,6 +17,7 @@ const app = new Hono()
 app.use(cors())
 
 let count = 0
+let clientid = 0
 
 app.get("/api", (c) => {
   return c.text("Hi! " + count)
@@ -25,22 +27,34 @@ app.get("/count", (c) => {
   return c.text(count.toString())
 })
 
-let webby: WSContext;
+let webby: Map<string, WSContext> = new Map();
 
 app.post("/count", async (c) => {
+
   let text = await c.req.text()
   count = parseInt(text)
-  webby.send(JSON.stringify({
-    msg: "Here's the count!",
-    count: count
-  }))
+  for (let value of webby.values()) {
+    value.send(JSON.stringify({
+      msg: "Here's the count! " + count,
+      count: count
+    }))
+  }
   return c.text(count.toString())
 })
 
 app.use("/socket", upgradeWebSocket((c) => {
   return {
     onOpen: async (event, ws) => {
-      webby = ws
+      const clientid = getCookie(c, 'clientid')
+      if (!clientid) {
+        ws.send(JSON.stringify({
+          msg: "No clientid",
+          count: count,
+        }))
+        return
+      }
+
+      webby.set(clientid, ws)
     },
     onMessage: async (event, ws) => {
       let msg = event.data.toString()
@@ -63,6 +77,17 @@ app.use("/socket", upgradeWebSocket((c) => {
   }
 }))
 
+app.use("/*", (c, next) => {
+  const existing = getCookie(c, 'clientid')
+  if (!existing) {
+    setCookie(c, 'clientid', clientid.toString(), {
+      httpOnly: true,
+    })
+    clientid++
+  }
+
+  return next()
+})
 
 app.use("/*", serveStatic({ root: "../client/dist" }))
 
